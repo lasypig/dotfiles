29fae8c15b1d996c25df83e004ef3536333d834f
wangxb <wangxb@hisome.com>
2020-10-15 11:40:11 +0800

    [UPD] plug.vim

--- updatefile.sh
+++ updatefile.sh
@@ -5,3 +5,5 @@ echo "Updating ~/.config/nvim/init.vim ..."
 cp ~/.config/nvim/init.vim ./nvim
 echo "Updating ~/.config/nvim/autoload/shy.vim ..."
 cp ~/.config/nvim/autoload/shy.vim ./nvim/autoload
+echo "Updating ~/.config/nvim/autoload/plug.vim ..."
+cp ~/.config/nvim/autoload/plug.vim ./nvim/autoload
